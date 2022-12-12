eaa8aa6b7500892205c7d2d5a2452253d07f9be5
github peterdo415 <btsmanne@gmail.com>
2022-12-12 22:22:59 +0900

    add ransackとpagyでの検索機能

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,7 @@
 class SpacesController < ApplicationController
   def index
+    @q = Space.ransack(params[:q])
+    @pagy, @spaces = pagy(@q.result.with_attached_images.includes(:features).order(created_at: :desc))
   end
 
   def new
