dfe2b14a119623887eee28a3a4858ff7b246f50d
github peterdo415 <btsmanne@gmail.com>
2022-12-16 16:02:07 +0900

    add 新規space作成機能

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -5,12 +5,41 @@ class SpacesController < ApplicationController
   end
 
   def new
-    @space = Space.new
+    @space = Space.new(space_params)
+    if @space.save
+      redirect_to @space, success: '作成しました'
+    else
+      flash.now[:error] = '失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @space = Spaces.new
   end
 
   def edit
+    @space = Space.find(params[:id])
+  end
+
+  def update
+    @space = Spaces.find(params[:id])
+    if @space.update(space_params)
+      redirect_to @space, success: '更新しました'
+    else
+      flash.now[:error] = '失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def show
+    @space = Space.find(params[:id])
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :description, :address, :nearest_station, { space_type_ids: [] },
+                                  { feature_ids: [] }, :longitude, :latitude, { images: []})
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,7 +3,25 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="map"
 export default class extends Controller {
 
+  static values = {
+    latitude: Number,
+    longitude: Number
+  }
 
   connect() {
+    this.renderMap(this.latitudeValue, this.longitudeValue)
+  }
+
+  renderMap(latitude, longitude) {
+    const targetLocation = { lat: latitude, lng: longitude}
+    const map = new google.maps.Map(this.element, {
+      center: targetLocation,
+      zoom: 16
+    });
+
+    new google.maps.Makers({
+      position: targetLocation,
+      map: map
+    });
   }
 }
